42640020ea642ee86bf43cd6bfe4f412c6562c32
AustinjMinnon <whi7ee@gmail.com>
2016-02-22 19:25:24 -0800

    add spec definition instantiates correctly

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,2 +1,23 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+
+public class Definition {
+  private static ArrayList<Definition> instances = new ArrayList<Definition>();
+
+  private String mDescription;
+  private LocalDateTime mCreatedAt;
+  private boolean mCompleted;
+  private int mId;
+
+  public Definition(String description) {
+    mDescription = description;
+    mCreatedAt = LocalDateTime.now();
+    mCompleted = false;
+    instances.add(this);
+    mId = instances.size();
+  }
+
+  public String getDescription() {
+    return mDescription;
+  }
+}
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -5,4 +5,12 @@ import static org.junit.Assert.*;
 
 public class DefinitionTest {
 
+  @Rule
+   public ClearRule clearRule = new ClearRule();
+
+  @Test
+  public void definition_instantiatesCorrectly_true() {
+    Definition myDefinition = new Definition("Mow the lawn");
+    assertEquals(true, myDefinition instanceof Definition);
+  }
 }
