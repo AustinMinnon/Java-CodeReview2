326b24017a5ffda3ea0dd2e8cee7318da815377e
AustinjMinnon <whi7ee@gmail.com>
2016-02-22 19:47:31 -0800

    add spec returns Word Id

--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -10,4 +10,9 @@ public class WordTest {
     Word testWord = new Word("Home");
     assertEquals("Home", testWord.getName());
   }
+  @Test
+  public void getId_returnsWordId() {
+    Word testWord = new Word("Home");
+    assertTrue(Word.all().size() == testWord.getId());
+  }
 }
